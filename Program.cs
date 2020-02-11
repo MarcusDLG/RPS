aea95dd4a1a9a3c86722ba3925d1ffeb16f4342e
git config --global hub.protocol https <>
2020-02-11 14:58:17 -0500

    added adventure mode with normal, impossible, and easy modes. Included play again feature.

--- Program.cs
+++ Program.cs
@@ -8,56 +8,129 @@ namespace RPS
     {
       // As a user, I should see a welcome message.
       Console.WriteLine("Welcome to Rock Paper Scissors");
-      //  As a user, I should see the options I can pick (i.e., "rock", "paper", "scissor")
-      Console.WriteLine("Please pick from the following: rock, paper, or scissors.");
-      var userChoice = Console.ReadLine().ToLower();
-      Console.WriteLine("You chose: " + userChoice);
-      //Add a validation tool to verify that user put in correct type of data
-      if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors")
+      bool flag = true; //need to figure out WHY this works. Found on stack overflow, but no understanding how the bool flag works
+      while (flag)
       {
-        Console.WriteLine("That is not a valid choice, chose again from rock, paper, or scissors.");
-        userChoice = Console.ReadLine().ToLower();
-      }
-      //  The computer should randomly decide one of the options and store as a variable.
-      Random rnd = new Random();
-      string[] answer = { "rock", "paper", "scissors" };
+        //User should be prompted to choose a level: 
+        Console.WriteLine("Please choose a difficulty: normal, impossible, and easy.");
+        var difficultyType = Console.ReadLine().ToLower();
+        if (difficultyType != "normal" && difficultyType != "impossible" && difficultyType != "easy")
+        {
+          Console.WriteLine("That is not a valid choice, chose again from normal, impossible, and easy.");
+          difficultyType = Console.ReadLine().ToLower();
+        }
+        if (difficultyType == "normal")
+        {
+          //  As a user, I should see the options I can pick (i.e., "rock", "paper", "scissor")
+          Console.WriteLine("Please pick from the following: rock, paper, or scissors.");
+          var userChoice = Console.ReadLine().ToLower();
+          Console.WriteLine("You chose: " + userChoice);
+          //Add a validation tool to verify that user put in correct type of data
+          if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors")
+          {
+            Console.WriteLine("That is not a valid choice, chose again from rock, paper, or scissors.");
+            userChoice = Console.ReadLine().ToLower();
+          }
+          //  The computer should randomly decide one of the options and store as a variable.
+          Random rnd = new Random();
+          string[] answer = { "rock", "paper", "scissors" };
 
-      int rpsIndex = rnd.Next(answer.Length);
-      var computerChoice = answer[rpsIndex];
+          int rpsIndex = rnd.Next(answer.Length);
+          var computerChoice = answer[rpsIndex];
 
-      //I need to tell it how each variable is equal to, greater than, or less than the others. 
-      if (userChoice == computerChoice)
-      {
-        Console.WriteLine("Congrats, you both chose " + userChoice + ". It is a draw.");
-      }
-      else if (userChoice == "rock" && computerChoice == "paper")
-      {
-        Console.WriteLine("Sorry, the computer chose: " + computerChoice + ". You lose.");
-      }
-      else if (userChoice == "rock" && computerChoice == "scissors")
-      {
-        Console.WriteLine("Congrats you win: you chose " + userChoice + " and the computer chose " + computerChoice + ".");
-      }
-      else if (userChoice == "paper" && computerChoice == "scissors")
-      {
-        Console.WriteLine("Sorry, the computer chose: " + computerChoice + ". You lose.");
-      }
-      else if (userChoice == "paper" && computerChoice == "rock")
-      {
-        Console.WriteLine("Congrats you win: you chose " + userChoice + " and the computer chose " + computerChoice + ".");
-      }
-      else if (userChoice == "scissors" && computerChoice == "paper")
-      {
-        Console.WriteLine("Congrats you win: you chose " + userChoice + " and the computer chose " + computerChoice + ".");
-      }
-      else if (userChoice == "scissors" && computerChoice == "rock")
-      {
-        Console.WriteLine("Congrats you win: you chose " + userChoice + " and the computer chose " + computerChoice + ".");
+          //I need to tell it how each variable is equal to, greater than, or less than the others. 
+          if (userChoice == computerChoice)
+          {
+            Console.WriteLine("Congrats, you both chose " + userChoice + ". It is a draw.");
+          }
+          else if (userChoice == "rock" && computerChoice == "paper")
+          {
+            Console.WriteLine("Sorry, the computer chose: " + computerChoice + ". You lose.");
+          }
+          else if (userChoice == "rock" && computerChoice == "scissors")
+          {
+            Console.WriteLine("Congrats you win: you chose " + userChoice + " and the computer chose " + computerChoice + ".");
+          }
+          else if (userChoice == "paper" && computerChoice == "scissors")
+          {
+            Console.WriteLine("Sorry, the computer chose: " + computerChoice + ". You lose.");
+          }
+          else if (userChoice == "paper" && computerChoice == "rock")
+          {
+            Console.WriteLine("Congrats you win: you chose " + userChoice + " and the computer chose " + computerChoice + ".");
+          }
+          else if (userChoice == "scissors" && computerChoice == "paper")
+          {
+            Console.WriteLine("Congrats you win: you chose " + userChoice + " and the computer chose " + computerChoice + ".");
+          }
+          else if (userChoice == "scissors" && computerChoice == "rock")
+          {
+            Console.WriteLine("Congrats you win: you chose " + userChoice + " and the computer chose " + computerChoice + ".");
 
+          }
+        }
+        else if (difficultyType == "impossible")
+        {
+          //  As a user, I should see the options I can pick (i.e., "rock", "paper", "scissor")
+          Console.WriteLine("Please pick from the following: rock, paper, or scissors.");
+          var userChoice = Console.ReadLine().ToLower();
+          Console.WriteLine("You chose: " + userChoice);
+          //Add a validation tool to verify that user put in correct type of data
+          if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors")
+          {
+            Console.WriteLine("That is not a valid choice, chose again from rock, paper, or scissors.");
+            userChoice = Console.ReadLine().ToLower();
+          }
+          //I need it to inform the user that they've lost regardless of  their choice
+          if (userChoice == "rock")
+          {
+            Console.WriteLine("Sorry, the computer chose paper. You lose.");
+          }
+          else if (userChoice == "paper")
+          {
+            Console.WriteLine("Sorry, the computer chose scissors. You lose.");
+          }
+          else if (userChoice == "scissors")
+          {
+            Console.WriteLine("Sorry the computer chose rock. You lose");
+          }
+        }
+        else if (difficultyType == "easy")
+        {
+          //  As a user, I should see the options I can pick (i.e., "rock", "paper", "scissor")
+          Console.WriteLine("Please pick from the following: rock, paper, or scissors.");
+          var userChoice = Console.ReadLine().ToLower();
+          Console.WriteLine("You chose: " + userChoice);
+          //Add a validation tool to verify that user put in correct type of data
+          if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors")
+          {
+            Console.WriteLine("That is not a valid choice, chose again from rock, paper, or scissors.");
+            userChoice = Console.ReadLine().ToLower();
+          }
+          //I need it to inform the user that they've won regardless of  their choice
+          if (userChoice == "rock")
+          {
+            Console.WriteLine("You win, the computer chose scissors");
+          }
+          else if (userChoice == "paper")
+          {
+            Console.WriteLine("You win, the computer chose rock.");
+          }
+          else if (userChoice == "scissors")
+          {
+            Console.WriteLine("You win, the computer chose paper.");
+          }
+        }
+        Console.WriteLine("Play again? ('yes' or 'no')");
+        string playAgain = null;
+        playAgain = Console.ReadLine();
+        if (playAgain == "no")
+          flag = false;
       }
     }
   }
 }
+
 
 
 
